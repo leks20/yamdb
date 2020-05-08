dcd74b5e1cba068f291925ae2b3c52601c782f14
Евгений Ахметзянов <eugenueakhmetzyanov@MBP-Evgenij.dftnet>
2020-05-08 17:41:48 +0500

    tiny fix

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
